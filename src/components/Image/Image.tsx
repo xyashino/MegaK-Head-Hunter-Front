7dfd0e1ca95284b48bd5017b5720e7ff4e1af491
Kasia <kajzerkatarzyna1@gmail.com>
2023-04-19 02:44:58 +0200

    Added the image "logo.png" to "assets" and imported it into "Image.tsx", created file "Image.css" and made a stylesheet for "Image.tsx".

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import './Image.css';
+import img from "@assets/logo.png"
 
 export const Image = () => {
   return (
-    <>
-      <img src="" alt="" />
-    </>
+    <div className="div-img-wrapper">
+      <img className="img-logo" src={img} alt="Logo kursu MegaK" />
+    </div>
   );
 };
