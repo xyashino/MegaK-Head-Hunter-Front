0550eee7d97add379a63c0a4ebb343d6a5fea984
Patryk Zwolak <patrykzwolak.pz@gmail.com>
2023-04-21 17:44:04 +0200

    added error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { LoginPage } from "@pages/Login/LoginPage";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { ErrorPage } from "@pages/Error/ErrorPage";
 
 const routers = createBrowserRouter([
     {
         path:'*',
         element: <LoginPage />,
+    },
+    {
+        path:'/error',
+        element: <ErrorPage title="Błąd" message="błąd krytyczny, wyrzuć komputer przez okno"/>
     }
 ]);
 export const App = () =>  <RouterProvider router={routers} />;
---
+++ src/pages/Error/ErrorPage.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Link} from "react-router-dom"
+import classes from "./ErrorPage.module.css"
+import img from "@assets/error-image.png"
+
+type Props = {
+  title: string,
+  message: string,
+}
+
+export const ErrorPage = (props: Props) => {
+  return <div className={classes.ErrorPage}>
+    <div className={classes.title}><h2>{props.title}</h2></div>
+    <hr />
+    <p className={classes.message}>{props.message}</p>
+    <div className={classes.container}>
+      <div className={classes.link}>
+        <Link to='/' className={classes.backLink}>back to main site</Link>
+      </div>
+      <div className={classes.imageContainer}>
+        <img src={img} alt="error image"/>
+      </div>
+    </div>
+  </div>
+}
