b7c99af3bb860de148cbcc9b7a78d2574a7bc4cc
Mikolaj Dutkiewicz <mikolajdutkiewicz@gmail.com>
2023-04-21 19:14:02 +0200

    Update component logic and style

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,47 @@
 import { Avatar } from "@components/Avatar/Avatar";
 import { Logo } from "@components/Logo/Logo";
 import { Paragraph } from "@components/Paragraph/Paragraph";
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Navbar.module.css";
+import { Link } from "react-router-dom";
 
-const githubUsername = 'Ami777'; // to trzeba zastąpić githubUsername z BE/formularza FE
- 
+const githubUsername = "Ami777"; // to trzeba zastąpić
 
 export const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
     <nav>
-      <div className={classes.nav_container}>
-        <Logo />
+      <div
+        className={`${classes.nav_container} ${
+          showMenu ? classes.showmenu : ""
+        }`}
+      >
+        <Paragraph>Logo</Paragraph>
         <div className={classes.nav_user}>
-          <Avatar />
-          <Paragraph>{githubUsername}</Paragraph>
-          <span className="material-icons">arrow_drop_down</span>     
+          <button className={classes.info} onClick={toggleMenu}>
+            <Avatar/>
+            <Paragraph
+              style={{
+                font: "normal normal normal 18px/30px Catamaran;",
+                marginRight: "auto",
+              }}
+            >
+              {githubUsername}
+            </Paragraph>
+            <span className="material-icons" style={{ color: "#9e9e9e" }}>
+              arrow_drop_down
+            </span>
+          </button>
+          <div
+            className={`${classes.modal} ${showMenu ? classes.showmenu : ""}`}
+          >
+            <Link to="#">Konto</Link>
+            <Link to="#">Wyloguj</Link>
+          </div>
         </div>
       </div>
     </nav>
