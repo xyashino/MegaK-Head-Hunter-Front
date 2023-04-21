b05aec781bf27202268ed5a0e16f35fa98c17974
Mikolaj Dutkiewicz <mikolajdutkiewicz@gmail.com>
2023-04-21 18:05:33 +0200

    Import css file to Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,19 @@
+import { Avatar } from "@components/Avatar/Avatar";
+import { Logo } from "@components/Logo/Logo";
+import { Paragraph } from "@components/Paragraph/Paragraph";
 import React from "react";
+import classes from "./Navbar.module.css"
 
 export const Navbar = () => {
-  return <>Navbar</>;
+  return (
+    <nav>
+      <div className={classes.navbar_container}>
+        <Logo />
+        <div className={classes.navbar_user}>
+          <Avatar />
+          <Paragraph></Paragraph>
+        </div>
+      </div>
+    </nav>
+  );
 };
