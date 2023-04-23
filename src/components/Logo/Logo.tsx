947acf23b1bfdf8f95c21a9f8e3f1c8869968414
Mikolaj Dutkiewicz <mikolajdutkiewicz@gmail.com>
2023-04-23 12:16:43 +0200

    Added TS props to Logo component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,11 +1,19 @@
-import React from "react";
-import classes from './Logo.module.css';
-import img from "@assets/logo.png"
+import React, { HTMLAttributes } from "react";
+import classes from "./Logo.module.css";
+import img from "@assets/logo.png";
 
-export const Logo = () => {
+type Props = HTMLAttributes<HTMLImageElement>
+
+export const Logo = ({ ...rest }: Props) => {
   return (
     <div className={classes.div_img_wrapper}>
-      <img className={classes.img_logo} src={img} alt="Logo kursu MegaK" draggable={false}/>
+      <img
+        className={classes.img_logo}
+        src={img}
+        alt="Logo kursu MegaK"
+        {...rest}
+        draggable={false}
+      />
     </div>
   );
 };
