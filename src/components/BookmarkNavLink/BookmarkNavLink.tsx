d6424a619e2f0366614c3d66cc349dcf7fd34008
Kasia <kajzerkatarzyna1@gmail.com>
2023-04-24 21:48:29 +0200

    Removed const variables, added toggle classes to style.

--- src/components/BookmarkNavLink/BookmarkNavLink.tsx
+++ src/components/BookmarkNavLink/BookmarkNavLink.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import classes from './BookmarkNavLink.module.css';
 import {NavLink} from 'react-router-dom';
 
-const red = "var(--megakRed)";
-const bgInput = "var(--bgInput)";
-const white = "var(--white)";
-const inactive = "var(--inactiveBookmark)";
-
 interface LinkToBookmarkProps {
     text?: string;
     to: string;
@@ -17,13 +12,9 @@ export const BookmarkNavLink = ({ text, to }: LinkToBookmarkProps) => {
 
         <NavLink
             to={to}
-            className={classes.bookmark_nav_link}
-            style={({ isActive }) => {
-                return {
-                    borderColor: isActive ? red : bgInput,
-                    color: isActive ? white : inactive,
-                };
-            }}
+            className={({ isActive, isPending }) =>
+                isActive ? classes.active : classes.pending
+            }
         >
             {text}
         </NavLink>
