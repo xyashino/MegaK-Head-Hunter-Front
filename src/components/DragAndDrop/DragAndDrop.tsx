49bc6cb12fd0af1b77071c802dbbb8fc41b9fb92
xyashino <szczypiorpan123@gmail.com>
2023-04-26 19:28:52 +0200

    Simplify code

--- src/components/DragAndDrop/DragAndDrop.tsx
+++ src/components/DragAndDrop/DragAndDrop.tsx
@@ -1,26 +1,26 @@
-import React, { ReactNode, useState} from 'react';
-import axios from "axios";
-import Dropzone from 'react-dropzone';
-import classes from "./DragAndDrop.module.css"
-import { AxiosProgressEvent } from 'axios';
+import React, { ReactNode, useState } from "react";
+import Dropzone from "react-dropzone";
+import classes from "./DragAndDrop.module.css";
+import { AxiosProgressEvent } from "axios";
+import { AxiosSetup } from "@utils/network/AxiosSetup";
+import { RequestPath } from "@enums/request-path.enum";
 
-  const fileUploadedMsg = <p className={classes.green}>Plik wysłany</p>
-  const fileUploadError = <p className={classes.red}>Wystąpił problem z wysłaniem pliku, spróbuj ponownie</p>
+const fileUploadedMsg = <p className={classes.green}>Plik wysłany</p>;
+const fileUploadError = (
+  <p className={classes.red}>
+    Wystąpił problem z wysłaniem pliku, spróbuj ponownie
+  </p>
+);
 
-const http = axios.create({
-  baseURL: "http://localhost:3000",
-  headers: {
-    "Content-type": "application/json",
-  },
-});
-
-
-const uploadOnServer = (file: File, onUploadProgress?: (progressEvent: AxiosProgressEvent) => void): Promise<any> => {
+const uploadOnServer = (
+  file: File,
+  onUploadProgress?: (progressEvent: AxiosProgressEvent) => void
+): Promise<any> => {
   let formData = new FormData();
 
   formData.append("uploadStudents", file);
 
-  return http.post("/upload/file", formData, {
+  return AxiosSetup.post(RequestPath.Upload, formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
@@ -29,7 +29,9 @@ const uploadOnServer = (file: File, onUploadProgress?: (progressEvent: AxiosProg
 };
 
 export const DragAndDrop = () => {
-  const [selectedFiles, setSelectedFiles] = useState<File[] | undefined>(undefined);
+  const [selectedFiles, setSelectedFiles] = useState<File[] | undefined>(
+    undefined
+  );
   const [currentFile, setCurrentFile] = useState<File | undefined>(undefined);
   const [message, setMessage] = useState<ReactNode | string>("");
 
@@ -39,37 +41,33 @@ export const DragAndDrop = () => {
     }
   };
 
-
   const upload = () => {
-    if (selectedFiles && selectedFiles.length > 0){
-    let currentFile = selectedFiles[0];
+    if (selectedFiles && selectedFiles.length > 0) {
+      let currentFile = selectedFiles[0];
 
-    setCurrentFile(currentFile);
+      setCurrentFile(currentFile);
+      uploadOnServer(currentFile)
+        .then(() => {
+          setMessage(fileUploadedMsg);
+        })
+        .catch(() => {
+          setMessage(fileUploadError);
+          setCurrentFile(undefined);
+        });
 
-            uploadOnServer(currentFile)
-              .then(() => {
-                setMessage(fileUploadedMsg);
-              })
-              .catch(() => {
-              setMessage(fileUploadError);
-              setCurrentFile(undefined);
-              });
-
-    setSelectedFiles(undefined);
-  }};
-
+      setSelectedFiles(undefined);
+    }
+  };
 
   return (
     <div className={classes.box}>
       <Dropzone onDrop={onDrop} multiple={false}>
         {({ getRootProps, getInputProps }) => (
           <section>
-            <div  className={classes.drop_box} {...getRootProps()}>
+            <div className={classes.drop_box} {...getRootProps()}>
               <input {...getInputProps()} />
               {selectedFiles && selectedFiles[0] && selectedFiles[0].name ? (
-                <div>
-                  {selectedFiles[0].name}
-                </div>
+                <div>{selectedFiles[0].name}</div>
               ) : (
                 "Przeciągnij i upuść plik tutaj lub kliknij, aby wybrać plik"
               )}
@@ -86,9 +84,7 @@ export const DragAndDrop = () => {
           </section>
         )}
       </Dropzone>
-      <div>
-        {message}
-      </div>
+      <div>{message}</div>
     </div>
   );
-  }
+};
