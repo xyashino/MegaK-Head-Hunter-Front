1f2615300297c2f9fc3514d287a8fba2d9204f87
xyashino <szczypiorpan123@gmail.com>
2023-04-26 15:00:54 +0200

    Created a new enum named `PagerRouter` for storing PagerRouter dat

---
+++ src/enums/page-router.enum.ts
@@ -0,0 +1,9 @@
+export enum PageRouter {
+  Main = "/",
+  Login = "/login",
+  Admin = "/admin",
+  AdminHr = "/admin/hr",
+  AdminStudents = "/admin/students",
+  Error = "/error",
+  Everything = "*",
+}
