a7cb9fb474ca9b14e6d54368fb2553d61f4dbd44
xyashino <szczypiorpan123@gmail.com>
2023-04-26 15:13:38 +0200

    Implemented the `logout` method to allow users to log out of the application

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,35 @@
 import { Avatar } from "@components/Avatar/Avatar";
 import { Logo } from "@components/Logo/Logo";
 import { Paragraph } from "@components/Paragraph/Paragraph";
-import React, { useState } from "react";
+import React, { SyntheticEvent, useState } from "react";
 import classes from "./Navbar.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "@components/Button/Button";
+import { PageRouter } from "@enums/page-router.enum";
+import { useAxios } from "@hooks/useAxios";
+import { RequestPath } from "@enums/request-path.enum";
 
-const githubUsername = "Ami777"; // to trzeba zastąpić
-
-export const Navbar = () => {
+interface Props {
+  githubUsername?: string;
+  firstName: string;
+  lastName: string;
+}
+export const Navbar = ({ githubUsername, firstName, lastName }: Props) => {
   const [showMenu, setShowMenu] = useState(false);
-
+  const navigate = useNavigate();
+  const { fetchData } = useAxios({ url: RequestPath.Logout, method: "DELETE" });
   const toggleMenu = () => {
-    setShowMenu(!showMenu);
+    setShowMenu((prevState) => !prevState);
   };
+
+  const logoutUser = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const result = confirm("Czy na pewno chcesz się wylogowac?");
+    if (result) {
+      await fetchData(()=>{navigate(PageRouter.Login)});
+    }
+  };
+
   return (
     <nav>
       <div
@@ -20,17 +37,19 @@ export const Navbar = () => {
           showMenu ? classes.showmenu : ""
         }`}
       >
-        <Logo style={{ marginBottom: 0, justifyContent: "start", height: "55px" }} />
+        <Logo
+          style={{ marginBottom: 0, justifyContent: "start", height: "55px" }}
+        />
         <div className={classes.nav_user}>
           <button className={classes.info} onClick={toggleMenu}>
-            <Avatar />
+            <Avatar githubUsername={githubUsername} />
             <Paragraph
               style={{
                 font: "normal normal normal 18px/30px Catamaran",
                 marginRight: "auto",
               }}
             >
-              {githubUsername}
+              {`${firstName} ${lastName}`}
             </Paragraph>
             <span className="material-icons" style={{ color: "#9e9e9e" }}>
               arrow_drop_down
@@ -40,7 +59,7 @@ export const Navbar = () => {
             className={`${classes.modal} ${showMenu ? classes.showmenu : ""}`}
           >
             <Link to="#">Konto</Link>
-            <Link to="#">Wyloguj</Link>
+            <Button onClick={logoutUser}>Wyloguj</Button>
           </div>
         </div>
       </div>
