4c02d35dc8ca1d61bf5bce80c810fdabd47988fa
Kasia <kajzerkatarzyna1@gmail.com>
2023-04-27 12:28:00 +0200

    Changed component <Button/> to be more universal - className={className} - className comes from props.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,10 @@
 import React, { HTMLAttributes, PropsWithChildren } from "react";
-import classes from "./Button.module.css";
 
 interface Props extends HTMLAttributes<HTMLButtonElement>, PropsWithChildren {}
 
 export const Button = ({ className, style, children, ...rest }: Props) => {
   return (
-    <button {...rest} className={classes.button}>
+    <button {...rest} className={className}>
       {children}
     </button>
   );
