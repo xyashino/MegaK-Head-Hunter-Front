8e6bf64af1a3b6dddddca77d571f0f72bc49d8f4
xyashino <szczypiorpan123@gmail.com>
2023-04-30 21:53:30 +0200

    Create 'Toggle' component for UI toggling functionality

---
+++ src/components/Toggle/Toggle.tsx
@@ -0,0 +1,30 @@
+import React,{ useLayoutEffect, useState } from "react";
+import classes from "./Toggle.module.css";
+
+interface Props {
+  runAfterChange: (isOn: boolean) => void;
+  defaultValue?: boolean;
+}
+
+export const Toggle = ({ runAfterChange, defaultValue = false }: Props) => {
+  const [isOn, setIsOn] = useState(defaultValue);
+
+  useLayoutEffect(() => {
+    runAfterChange(isOn);
+  }, [isOn]);
+  const handleChange = () => {
+    setIsOn((prevIsOn) => !prevIsOn);
+  };
+
+  return (
+    <div className={classes.toggle}>
+      <input
+        type="checkbox"
+        checked={isOn}
+        onChange={handleChange}
+        id="toggle-switch"
+      />
+      <label htmlFor="toggle-switch"></label>
+    </div>
+  );
+};
