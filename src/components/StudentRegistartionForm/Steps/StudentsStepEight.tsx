74fca1fc3a4544ae89a7f10fd504844173213e89
xyashino <szczypiorpan123@gmail.com>
2023-05-01 13:38:42 +0200

    Refactor code

--- src/components/StudentRegistartionForm/Steps/StudentsStepEight.tsx
+++ src/components/StudentRegistartionForm/Steps/StudentsStepEight.tsx
@@ -1,31 +1,72 @@
-import React,{SyntheticEvent, useContext, useLayoutEffect, useState} from "react";
+import React, {
+  SyntheticEvent,
+  useContext,
+  useLayoutEffect,
+  useState,
+} from "react";
 import { TextArea } from "@components/TextArea/TextArea";
 import { Button } from "@components/Button/Button";
 import { Text } from "@components/Text/Text";
 import { StudentRegistrationContext } from "@context/StudentRegistrationContext";
 import classes from "../StudentRegistrationForm.module.css";
-export const StudentsStepEight = () => {
-  const { studentData,setStudentData } = useContext(StudentRegistrationContext);
+import { useAxios } from "@hooks/useAxios";
+import { RequestPath } from "@enums/request-path.enum";
+import { useNavigate, useParams } from "react-router-dom";
+import { PageRouter } from "@enums/page-router.enum";
+import { StudentRegisterRequest } from "@backendTypes";
 
-  const [experience ,setExperience] = useState(studentData.workExperience)
-  const handleClick = (e: SyntheticEvent) => {
+const processStudentData = <T extends StudentRegisterRequest>(obj: T): Partial<T> => {
+  const filteredStudentData: Partial<T> = {};
+  for (const key in obj) {
+    if (obj[key]) {
+      filteredStudentData[key] = obj[key];
+    }
+  }
+  filteredStudentData.canTakeApprenticeship ??= false;
+  if (filteredStudentData.monthsOfCommercialExp === 0) {
+      delete filteredStudentData.monthsOfCommercialExp;
+  }
+  return filteredStudentData;
+};
+export const StudentsStepEight = () => {
+  const { studentData, setStudentData } = useContext(
+    StudentRegistrationContext
+  );
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { fetchData } = useAxios({
+    body: processStudentData(studentData),
+    url: `${RequestPath.CreteStudent}${id}`,
+    method: "POST",
+  });
+
+  const [experience, setExperience] = useState(studentData.workExperience);
+  const handleClick = async (e: SyntheticEvent) => {
     e.preventDefault();
-    // console.log(Object.keys(studentData).reduce((acc, k) => (!studentData[k] && delete acc[k], acc), studentData));
+    await fetchData(() => {
+      navigate(PageRouter.Main);
+    });
   };
 
-  useLayoutEffect(()=>{
-      setStudentData(prevState => ({...prevState,workExperience:experience}))
-  },[experience])
+  useLayoutEffect(() => {
+    setStudentData((prevState) => ({
+      ...prevState,
+      workExperience: experience,
+    }));
+  }, [experience]);
 
-  const handleInputChange = (e:SyntheticEvent)=>{
-      e.preventDefault();
-      const {value} = e.target as HTMLTextAreaElement;
-      setExperience(value)
-  }
+  const handleInputChange = (e: SyntheticEvent) => {
+    e.preventDefault();
+    const { value } = e.target as HTMLTextAreaElement;
+    setExperience(value);
+  };
 
   return (
     <>
-      <TextArea description="Przebieg doświadczenia zawodowego" onChange={handleInputChange}/>
+      <TextArea
+        description="Przebieg doświadczenia zawodowego"
+        onChange={handleInputChange}
+      />
       <Button customClasses={classes.activate_btn}>
         <Text weight="bold" onClick={handleClick}>
           Aktywuj konto
