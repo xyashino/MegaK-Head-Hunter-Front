30f7d653d50a4f1847cb2a89aaa7d19d9e21dd37
xyashino <szczypiorpan123@gmail.com>
2023-05-04 18:30:46 +0200

    Prettify code

--- src/App.tsx
+++ src/App.tsx
@@ -53,8 +53,7 @@ const routers = createBrowserRouter([
   },
   {
     path: PageRouter.StudentRegistration,
-    loader: ({ params }) =>
-        getDataFrom(`${RequestPath.GetOneStudent}${params.id}`),
+    loader: ({ params }) => getDataFrom(`${RequestPath.GetOneStudent}${params.id}`),
     element: <StudentRegistration />,
   },
   {
