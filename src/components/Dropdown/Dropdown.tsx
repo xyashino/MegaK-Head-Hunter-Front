7339a49710aac16218bcf10464fd346b94486bee
Mikolaj Dutkiewicz <mikolajdutkiewicz@gmail.com>
2023-05-05 18:27:15 +0200

    Logic improvement, CSS improvement, icon added

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import classes from "./Dropdown.module.css";
 
-interface DropdownProps {
-  title?: string | JSX.Element | JSX.Element[];
+interface DropdownProps { 
   children: string | JSX.Element | JSX.Element[];
+  firstOptionalBtn?: string | JSX.Element | JSX.Element[];
+  secondOptionalBtn?: string | JSX.Element | JSX.Element[];
+  reservationData?: string | JSX.Element | JSX.Element[];
+  userNameData?: string | JSX.Element | JSX.Element[];
+  userNameAvatarData?: string | JSX.Element | JSX.Element[];
 }
 
-export const Dropdown: React.FC<DropdownProps> = ({ title, children }) => {
+export const Dropdown: React.FC<DropdownProps> = ({ 
+  children,
+  firstOptionalBtn,
+  secondOptionalBtn,
+  reservationData,
+  userNameAvatarData,
+  userNameData,
+}) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDropdown = () => {
@@ -16,10 +27,35 @@ export const Dropdown: React.FC<DropdownProps> = ({ title, children }) => {
   return (
     <>
       <div className={classes.dropdown_container}>
-        <div className={classes.user_info}></div>
+        <div className={classes.user_info}>
+          {reservationData}
+          {userNameAvatarData}
+          {userNameData}
+        </div>
         <div className={classes.buttons_row}>
-          <button className={classes.dropdown_button} onClick={toggleDropdown}>
-            {title}
+          {firstOptionalBtn}
+          {secondOptionalBtn}
+          <button className={classes.dropdown_button}>
+            {isOpen ? (
+              <>
+                <img
+                  src="./arrow_down.svg"
+                  className={classes.icon}
+                  onClick={toggleDropdown}
+                  alt=""
+                />
+              </>
+            ) : (
+              <>
+                <img
+                  src="./arrow_down.svg"
+                  className={classes.icon}
+                  style={{ transform: "rotate(180deg)" }}
+                  onClick={toggleDropdown}
+                  alt=""
+                />
+              </>
+            )}
           </button>
         </div>
       </div>
