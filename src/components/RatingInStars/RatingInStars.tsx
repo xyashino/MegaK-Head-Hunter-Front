0407652528c8e3d0d5cbd8ca82f9c2265400b974
Mikolaj Dutkiewicz <mikolajdutkiewicz@gmail.com>
2023-05-06 15:39:03 +0200

    Assign colors to variables; removed unnecessary line

--- src/components/RatingInStars/RatingInStars.tsx
+++ src/components/RatingInStars/RatingInStars.tsx
@@ -1,21 +1,25 @@
-import {FaStar} from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
 import classes from "./RatingInStars.module.css";
 
 interface RatingInStarsProps {
-    numberOfStars: number;
+  numberOfStars: number;
 }
 
-export const RatingInStars = ({numberOfStars}: RatingInStarsProps) => {
-    return (
-        <span className={classes.stars}>
-            {[...Array(5)].map((star, i) => {
-                const value = i + 1;
-                return (
-                    <span key={i}>
-                        <FaStar color={value >= numberOfStars ? "grey" : "#e02635"}/>
-                    </span>
-                )
-            })}
-        </span>
-    )
+const GREY_COLOR = "gray";
+const RED_COLOR = "#e02635";
+
+export const RatingInStars = ({ numberOfStars }: RatingInStarsProps) => {
+  return (
+    <span className={classes.stars}>
+      {[...Array(5)].map((star) => {
+        return (
+          <span key={star}>
+            <FaStar
+              color={star + 1 >= numberOfStars ? GREY_COLOR : RED_COLOR}
+            />
+          </span>
+        );
+      })}
+    </span>
+  );
 };
