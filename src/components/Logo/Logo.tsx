a82d3b010db31813175710c676642862ad0957bc
xyashino <szczypiorpan123@gmail.com>
2023-05-07 16:29:59 +0200

    Add 'handleClick' method to navigate to main page

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,14 +1,25 @@
-import React, { HTMLAttributes } from "react";
-import classes from "./Logo.module.css";
 import img from "@assets/logo.png";
+import { PageRouter } from "@enums/page-router.enum";
+import { HTMLAttributes, SyntheticEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import classes from "./Logo.module.css";
 
 interface Props extends HTMLAttributes<HTMLImageElement> {
   customClasses?: string;
 }
 
 export const Logo = ({ customClasses = "", ...rest }: Props) => {
+  const navigate = useNavigate();
+  const handleClick = (e: SyntheticEvent) => {
+    e.preventDefault();
+    navigate(PageRouter.Main);
+  };
   return (
-    <div className={`${classes.div_img_wrapper} ${customClasses}`} {...rest}>
+    <div
+      className={`${classes.div_img_wrapper} ${customClasses}`}
+      {...rest}
+      onClick={handleClick}
+    >
       <img
         className={classes.img_logo}
         src={img}
