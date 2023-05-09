8a7e999d6c1d5f55081e6d1670138016024d8798
xyashino <szczypiorpan123@gmail.com>
2023-05-09 22:56:29 +0200

    Add 'fullSize' prop to Select component for toggling the '.full_size' class

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -7,15 +7,16 @@ interface Props extends Omit<HTMLAttributes<HTMLSelectElement> , 'onChange'>{
     onChange?: (e:SyntheticEvent) => void;
     value?:string;
     name?:string
+    fullSize?:boolean;
 }
 
-export const Select = ({ options, description, onChange ,...rest}: Props) => {
+export const Select = ({ options, description, onChange , fullSize=true ,...rest}: Props) => {
     const handleSelectChange = (e: SyntheticEvent) => {
         onChange && onChange(e);
     };
 
     return (
-        <div className={classes.select_group}>
+        <div className={`${classes.select_group} ${fullSize ? classes.full_size : ''}`}>
             {description && (
                 <span className={classes.description}>{description}:</span>
             )}
