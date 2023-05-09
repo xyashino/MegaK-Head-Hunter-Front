fd6fe148a889aaaeffdba29993666c9bac75f1eb
xyashino <szczypiorpan123@gmail.com>
2023-05-10 00:01:18 +0200

    Add searchbar and add mock FilterButton

--- src/pages/Students/StudentsPage.tsx
+++ src/pages/Students/StudentsPage.tsx
@@ -3,24 +3,32 @@ import { ActiveStudentResponse, ManyStudentResponse } from "@backendTypes";
 import { RequestPath } from "@enums/request-path.enum";
 import { QueryManagement } from "@components/QueryManagement/QueryMenagment";
 import { Dropdown } from "@components/Dropdown/Dropdown";
-import { useState } from "react";
-
+import React, { useState } from "react";
+import { SearchUsers } from "@components/Search/Search";
+import classes from "./StudentsPage.module.css"
+import {Button} from "@components/Button/Button";
 export const StudentsPage = () => {
   const { meta, data: activeStudents } = useLoaderData() as ManyStudentResponse;
   const [students, setStudents] = useState(activeStudents);
 
   return (
-    <QueryManagement
-      baseStudents={activeStudents}
-      meta={meta}
-      request={RequestPath.GetStudents}
-      updateStudents={(e: ActiveStudentResponse[]) => {setStudents(e)}}
-    >
-      {students.map((el) => (
-        <Dropdown userNameData={el.firstname} key={el.id}>
-          {el.lastname}
-        </Dropdown>
-      ))}
-    </QueryManagement>
+      <QueryManagement
+        baseStudents={activeStudents}
+        meta={meta}
+        request={RequestPath.GetStudents}
+        updateStudents={(e: ActiveStudentResponse[]) => {
+          setStudents(e);
+        }}
+      >
+          <div className={classes.search_bar}>
+              <SearchUsers/>
+              <Button>Filtrowanie</Button>
+          </div>
+        {students.map((el) => (
+          <Dropdown userNameData={el.firstname} key={el.id}>
+            {el.lastname}
+          </Dropdown>
+        ))}
+      </QueryManagement>
   );
 };
