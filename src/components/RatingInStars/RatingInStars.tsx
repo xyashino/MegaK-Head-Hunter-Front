0b6788b2d6cf0b3c4a9bbd09857fc48d5c12111d
xyashino <szczypiorpan123@gmail.com>
2023-05-09 14:30:02 +0200

    Implement Star component in RatingInStars for better display of student rating

--- src/components/RatingInStars/RatingInStars.tsx
+++ src/components/RatingInStars/RatingInStars.tsx
@@ -1,21 +1,18 @@
-import {FaStar} from "react-icons/fa";
 import classes from "./RatingInStars.module.css";
+import {Star} from "@components/RatingInStars/Star";
 
 interface RatingInStarsProps {
-    numberOfStars: number;
+  numberOfStars: number;
 }
 
-export const RatingInStars = ({numberOfStars}: RatingInStarsProps) => {
-    return (
-        <span className={classes.stars}>
-            {[...Array(5)].map((star, i) => {
-                const value = i + 1;
-                return (
-                    <span key={i}>
-                        <FaStar color={value >= numberOfStars ? "grey" : "#e02635"}/>
-                    </span>
-                )
-            })}
+export const RatingInStars = ({ numberOfStars }: RatingInStarsProps) => {
+  return (
+    <span className={classes.stars}>
+      {[...Array(5)].map((star, i) => (
+        <span key={i}>
+          <Star color={i + 1 >= numberOfStars ? "grey" : "#e02635"} fontSize={'24px'}/>
         </span>
-    )
+      ))}
+    </span>
+  );
 };
