81fdcc2f838afe88c6d4323b804f5b549db4b56a
Daniel Sulkowski <dansulk@gmail.com>
2023-05-10 14:55:57 +0200

    added filtration talks with useReducer

--- src/pages/Talk/TalksPage.tsx
+++ src/pages/Talk/TalksPage.tsx
@@ -5,6 +5,11 @@ import React, { useState } from "react";
 import { QueryManagement } from "@components/QueryManagement/QueryMenagment";
 import { Dropdown } from "@components/Dropdown/Dropdown";
 import { SearchUsers } from "@components/Search/Search";
+import classes from "@pages/Students/StudentsPage.module.css";
+import { Button } from "@components/Button/Button";
+import { Text } from "@components/Text/Text";
+import { FilterModal } from "@components/FilterModal/FilterModal";
+import { Avatar } from "@components/Avatar/Avatar";
 
 export const TalksPage = () => {
   const { data: activeStudents, meta } = useLoaderData() as {
@@ -12,6 +17,15 @@ export const TalksPage = () => {
     meta: PageMeta;
   };
   const [students, setStudents] = useState(activeStudents);
+  const [filterModalIsOpen, setFilterModalIsOpen] = useState<boolean>(false);
+
+  const openFilterModal = () => {
+    setFilterModalIsOpen(true);
+  };
+
+  const closeFilterModal = () => {
+    setFilterModalIsOpen(false);
+  };
 
   return (
     <QueryManagement
@@ -22,14 +36,58 @@ export const TalksPage = () => {
         setStudents(e);
       }}
     >
-      <div>
+      <div className={classes.search_bar}>
         <SearchUsers />
+        <div className={classes.div_btn_to_modal_wrapper}>
+          <Button
+            customClasses={classes.btn_to_modal}
+            onClick={openFilterModal}
+          >
+            <span
+              className="material-icons-outlined"
+              style={{
+                fontSize: "1.5rem",
+                verticalAlign: "-6px",
+                color: "#4D4D4D",
+                backgroundColor: "",
+              }}
+            >
+              filter_alt
+            </span>
+            Filtrowanie
+          </Button>
+        </div>
       </div>
       {students.map((el) => (
-        <Dropdown userNameData={el.firstname} key={el.id}>
-          {el.lastname}
-        </Dropdown>
+        <Dropdown
+          key={el.student.id}
+          reservationData={
+            <Text>
+              Rezerwacja do <br />
+              {new Date(el.bookingDate).toLocaleDateString()}
+            </Text>
+          }
+          userNameAvatarData={
+            <Avatar githubUsername={el.student.githubUsername} />
+          }
+          userNameData={
+            <Text>
+              {el.student.firstname} {el.student.lastname}
+            </Text>
+          }
+          children={
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At harum ipsum labore maxime natus nemo nostrum omnis quod ratione voluptate? Eum impedit ipsam obcaecati placeat quisquam rerum saepe sit. Debitis?"
+          }
+          firstOptionalBtn={<Button>Pokaż CV</Button>}
+          secondOptionalBtn={<Button>Brak zainteresowania</Button>}
+          thirdOptionalBtn={<Button>Zatrudniony</Button>}
+        />
       ))}
+      <FilterModal
+        isOpen={filterModalIsOpen}
+        onRequestClose={closeFilterModal}
+        onCancel={closeFilterModal}
+      />
     </QueryManagement>
   );
 };
