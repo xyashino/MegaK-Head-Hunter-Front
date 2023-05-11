7b8e4dbe5c9de184f3ef76dc8cc8a9e923bdd0e5
xyashino <szczypiorpan123@gmail.com>
2023-05-11 13:26:36 +0200

    Add simple validation in 'buildQueryUrl'

--- src/utils/buildQuery.ts
+++ src/utils/buildQuery.ts
@@ -1,8 +1,25 @@
-export const buildQueryUrl = (baseUrl: string, params: { [key:string]: string | number }) => {
-    const url = new URL(baseUrl);
-    for (const [key, value] of Object.entries(params)) {
-        url.searchParams.set(key, value as string);
+export const buildQueryUrl = (
+  baseUrl: string,
+  params: { [key: string]: string | string[] | boolean  | number}
+) => {
+  const url = new URL(baseUrl);
+  for (const [key, value] of Object.entries(params)) {
+    if (typeof value === "boolean") {
+      url.searchParams.set(key, value ? "1" : "0");
+      continue;
     }
-    return url.toString();
+    if (typeof value !== "number" && value.length === 0) {
+      url.searchParams.delete(key);
+      continue;
+    }
+    if (Array.isArray(value)) {
+      value.forEach((el) => {
+        if (el === "") return;
+        url.searchParams.append(key, el);
+      });
+      continue;
+    }
+    url.searchParams.set(key, value as string);
+  }
+  return url.toString();
 };
-
