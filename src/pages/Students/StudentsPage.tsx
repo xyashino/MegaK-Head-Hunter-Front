ba403b69e93006fc3e87a43fc80d222af8d74670
xyashino <szczypiorpan123@gmail.com>
2023-05-11 13:31:01 +0200

    Implement new FilterModal.tsx logic

--- src/pages/Students/StudentsPage.tsx
+++ src/pages/Students/StudentsPage.tsx
@@ -1,5 +1,5 @@
 import { useLoaderData } from "react-router-dom";
-import { ActiveStudentResponse, ManyStudentResponse } from "@backendTypes";
+import { ManyStudentResponse, ActiveStudentResponse } from "@backendTypes";
 import { RequestPath } from "@enums/request-path.enum";
 import { QueryManagement } from "@components/QueryManagement/QueryMenagment";
 import { Dropdown } from "@components/Dropdown/Dropdown";
@@ -9,19 +9,14 @@ import classes from "./StudentsPage.module.css";
 import { Button } from "@components/Button/Button";
 import { FilterModal } from "@components/FilterModal/FilterModal";
 import { Text } from "@components/Text/Text";
-import { CreateInterviewButton } from "@components/CreateInterviewButton/CreateInterviewButton";
 
 export const StudentsPage = () => {
   const { meta, data: activeStudents } = useLoaderData() as ManyStudentResponse;
-  const [students, setStudents] = useState(activeStudents);
-  const [filterModalIsOpen, setFilterModalIsOpen] = useState<boolean>(false);
+  const [students, setStudents] =
+    useState<ActiveStudentResponse[]>(activeStudents);
 
-  const openFilterModal = () => {
-    setFilterModalIsOpen(true);
-  };
-
-  const closeFilterModal = () => {
-    setFilterModalIsOpen(false);
+  const updateStudents = (e: ActiveStudentResponse[]) => {
+    setStudents(e);
   };
 
   return (
@@ -29,30 +24,12 @@ export const StudentsPage = () => {
       baseStudents={activeStudents}
       meta={meta}
       request={RequestPath.GetStudents}
-      updateStudents={(e: ActiveStudentResponse[]) => {
-        setStudents(e);
-      }}
+      updateStudents={updateStudents}
     >
       <div className={classes.search_bar}>
         <SearchUsers />
         <div className={classes.div_btn_to_modal_wrapper}>
-          <Button
-            customClasses={classes.btn_to_modal}
-            onClick={openFilterModal}
-          >
-            <span
-              className="material-icons-outlined"
-              style={{
-                fontSize: "1.5rem",
-                verticalAlign: "-6px",
-                color: "#4D4D4D",
-                backgroundColor: "",
-              }}
-            >
-              filter_alt
-            </span>
-            Filtrowanie
-          </Button>
+          <FilterModal />
         </div>
       </div>
       {students.map((el) => (
@@ -60,20 +37,17 @@ export const StudentsPage = () => {
           key={el.id}
           userNameData={
             <Text>
-              {el.firstname} {el.lastname.charAt(0) + "."}
+              {el.firstname} {el.lastname?.charAt(0) + "."}
             </Text>
           }
-          children={
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At harum ipsum labore maxime natus nemo nostrum omnis quod ratione voluptate? Eum impedit ipsam obcaecati placeat quisquam rerum saepe sit. Debitis?"
-          }
-          firstOptionalBtn={<CreateInterviewButton id={el.id} />}
-        />
+          firstOptionalBtn={<Button>Zarezerwuj rozmowę</Button>}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. At harum
+          ipsum labore maxime natus nemo nostrum omnis quod ratione voluptate?
+          Eum impedit ipsam obcaecati placeat quisquam rerum saepe sit.
+          Debitis?
+        </Dropdown>
       ))}
-      <FilterModal
-        isOpen={filterModalIsOpen}
-        onRequestClose={closeFilterModal}
-        onCancel={closeFilterModal}
-      />
     </QueryManagement>
   );
 };
