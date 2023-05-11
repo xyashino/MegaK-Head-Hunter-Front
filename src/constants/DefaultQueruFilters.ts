72b50621f0ed1e986a5d234a8f45573bd4d7f43f
xyashino <szczypiorpan123@gmail.com>
2023-05-11 13:07:25 +0200

    Create default data

---
+++ src/constants/DefaultQueruFilters.ts
@@ -0,0 +1,14 @@
+import {Filtration} from "@backendTypes";
+
+export const DEFAULT_QUERY_FILTERS: Filtration = {
+    courseCompletion: "",
+    courseEngagement: "",
+    projectDegree: "",
+    teamProjectDegree: "",
+    expectedTypeWork: "",
+    expectedContractType: "",
+    minSalary: "",
+    maxSalary: "",
+    canTakeApprenticeship: "",
+    monthsOfCommercialExp: "",
+};
