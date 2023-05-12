1509fb648143c601ae6b37414ee7ecd9c62e4f59
xyashino <szczypiorpan123@gmail.com>
2023-05-12 14:10:23 +0200

    Refactor code

---
+++ src/components/DropdownContent/DropdownContentRow.tsx
@@ -0,0 +1,14 @@
+import React, { PropsWithChildren } from "react";
+import classes from "./../Dropdown/Dropdown.module.css";
+interface Props extends PropsWithChildren {
+  title: string;
+}
+
+export const DropdownContentRow: React.FC<Props> = ({ title, children }) => (
+  <div className={classes.dropdown_row}>
+    <div className={classes.dropdown_row_title}>{title}</div>
+    <div className={classes.dropdown_row_data}>
+      {children}
+    </div>
+  </div>
+);
