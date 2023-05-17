4384f4c4009be6a766f7b8dcb79db7ebff37dc5a
xyashino <szczypiorpan123@gmail.com>
2023-05-17 15:23:43 +0200

    Sort imports

--- src/utils/checkAuth.ts
+++ src/utils/checkAuth.ts
@@ -1,14 +1,14 @@
+import { redirect } from "react-router-dom";
+import { toast } from "react-hot-toast";
 import { PageRouter } from "@enums/page-router.enum";
 import { RequestPath } from "@enums/request-path.enum";
 import { AxiosSetup } from "@utils/network/AxiosSetup";
-import { redirect } from "react-router-dom";
-import {toast} from "react-hot-toast";
+
 export const checkAuth = async () => {
   try {
-    const response = await AxiosSetup.get(RequestPath.Auth);
-    return response.data;
+    return (await AxiosSetup.get(RequestPath.Auth)).data;
   } catch (e) {
-    toast['error']('Musisz się zalogować')
+    toast.error("Musisz się zalogować");
     return redirect(PageRouter.Login);
   }
 };
